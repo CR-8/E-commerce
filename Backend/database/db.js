fb843460e7d52c28dc19ce31b54687557a15b91e
Pratyush <pratyushtiwari446@gmail.com>
2025-01-09 19:57:24 +0530

    Added Database
    Added database

---
+++ Backend/database/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const mongoURI ="mongodb://127.0.0.1:27017/todolist";
+
+const connectDB = () => {
+    mongoose.connect(mongoURI) 
+        .then(() => {
+            console.log("Connected to MongoDB");
+        })
+        .catch((err) => {
+            console.log("Error connecting to MongoDB", err);
+        }
+    )
+}
+
+export default connectDB;
